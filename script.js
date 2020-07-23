9805c6b9572f156b814109a4b355209f3ac37cd8
Mohit <mm8085574989@gmail.com>
2020-07-23 10:24:47 +0530

    Adding IndexDB

--- script.js
+++ script.js
@@ -1,38 +1,116 @@
-const ol = document.querySelector('ol');
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
 const input = document.querySelector('.input-area');
 const add = document.querySelector('.add');
-const rem = document.querySelector('.remove');
 var i = 0;
+let db;
 
-add.addEventListener('click', () => {
-    
-if(i<=8)    
+window.onload = function()
 {
-  var val = input.value; 
-  const li = document.createElement('li');
-  const span = document.createElement('span'); 
-  span.textContent = val;
-  li.appendChild(span);
-  ol.appendChild(li);
-  input.value = '';
-  input.focus();
-  i++;
-}
-else
-{
-    alert(`Limit reached delete Some TODO's`);
-    input.value = '';
-    i--;
-}
+  let request = window.indexedDB.open('notes_db', 1);
 
-});
+  request.onerror = function() {
+    console.log('Database failed to open');
+  };
 
-rem.addEventListener('click', () => {
+  request.onsuccess = function()
+  {
+    console.log('Database opened successfully');
+    db = request.result;
+    displayData();
+  };
+
+  request.onupgradeneeded = function(e) 
+  {
+    let objectStore = db.createObjectStore('notes_os', { keyPath: 'id', autoIncrement:true });
+    objectStore.createIndex('body', 'body', { unique: false });
+    console.log('Database setup complete');
+  };
+
+  add.onclick = addData;
+  
+
+  function addData(e  )
+  {
+    e.preventDefault();
+    let newItem = { body: input.value };
+    let transaction = db.transaction(['notes_os'], 'readwrite');
+    let objectStore = transaction.objectStore('notes_os');
+    let request = objectStore.add(newItem);
+    request.onsuccess = function() {
+      input.value = '';
+    };
+    transaction.oncomplete = function() {
+      console.log('Transaction completed: database modification finished.');
+      displayData();
+    };
+    transaction.onerror = function() {
+      console.log('Transaction not opened due to error');
+    };
+  }
+  
+
+  function displayData()
+  {
+    while(ul.firstChild)
+    {
+      ul.removeChild(ul.firstChild);
+    }  
+    let objectStore = db.transaction('notes_os').objectStore('notes_os');
+    objectStore.openCursor().onsuccess = function(e) {
+    let cursor = e.target.result;
+    if(cursor)
+    {
+        if(i<=7)    
+       {
+        const li = document.createElement('li');
+        const span = document.createElement('span'); 
+        const button = document.createElement('button');
+        button.setAttribute('class','remove');
+        span.textContent = val;
+        button.textContent = 'x';
+        li.setAttribute('data-note-id', cursor.value.id);
+        button.onclick = rem;
+        li.appendChild(span);
+        li.appendChild(button);
+        ul.appendChild(li);
+        input.value = '';
+        input.focus();
+        i++;
+        cursor.continue();
+       }
+       else
+       {
+        alert(`Either Limit reached delete Some TODO's or NUll value entered`);
+        input.value = '';
+        i--;
+       }
+    } 
+    else
+    {
+        if(!ul.firstChild) {
+         const li = document.createElement('li');
+         li.textContent = 'No notes stored.';
+         ul.appendChild(li);
+        }
+        console.log('Notes all displayed');
+    }
+   };     
+  }
     
-    var val = input.value;
-    // for(let j=0;j<=i;j++)
-    // {
-    //    if() 
-    // }
-
-});
+  function rem(e) {
+    let noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
+    let transaction = db.transaction(['notes_os'], 'readwrite');
+    let objectStore = transaction.objectStore('notes_os');
+    let request = objectStore.delete(noteId);
+    transaction.oncomplete = function() {
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+      console.log('Note ' + noteId + ' deleted.');
+      if(!ul.firstChild) {
+        let li = document.createElement('li');
+        li.textContent = 'No notes stored.';
+        ul.appendChild(li);
+      }
+    };
+  }
+}  
