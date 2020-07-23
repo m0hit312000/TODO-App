f27640f8a8c4cfb2b1e8dd2303ad50986aa3364f
Mohit <mm8085574989@gmail.com>
2020-07-23 12:13:00 +0530

    Changes done 2

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const ul = document.querySelector('ul');
 const form = document.querySelector('form');
-const input = document.querySelector('.input-area');
+const input = document.querySelector('#input-area');
 const add = document.querySelector('.add');
 var i = 0;
 let db;
 
 window.onload = function()
 {
-  let request = window.indexedDB.open('notes_db', 1);
+  let request = window.indexedDB.open('ds',2);
 
   request.onerror = function() {
     console.log('Database failed to open');
@@ -22,7 +22,8 @@ window.onload = function()
 
   request.onupgradeneeded = function(e) 
   {
-    let objectStore = db.createObjectStore('notes_os', { keyPath: 'id', autoIncrement:true });
+    let db = e.target.result;
+    let objectStore = db.createObjectStore('db', { keyPath: 'id', autoIncrement:true });
     objectStore.createIndex('body', 'body', { unique: false });
     console.log('Database setup complete');
   };
@@ -30,13 +31,12 @@ window.onload = function()
   add.onclick = addData;
   
 
-  function addData(e  )
+  function addData(e)
   {
-    e.preventDefault();
     let newItem = { body: input.value };
-    let transaction = db.transaction(['notes_os'], 'readwrite');
-    let objectStore = transaction.objectStore('notes_os');
-    let request = objectStore.add(newItem);
+    let transaction = db.transaction(['db'], 'readwrite');
+    let objectStore = transaction.objectStore('db');
+    var request = objectStore.add(newItem);
     request.onsuccess = function() {
       input.value = '';
     };
@@ -56,35 +56,25 @@ window.onload = function()
     {
       ul.removeChild(ul.firstChild);
     }  
-    let objectStore = db.transaction('notes_os').objectStore('notes_os');
+    let objectStore = db.transaction('db').objectStore('db');
     objectStore.openCursor().onsuccess = function(e) {
     let cursor = e.target.result;
     if(cursor)
     {
-        if(i<=7)    
-       {
+        
         const li = document.createElement('li');
-        const span = document.createElement('span'); 
         const button = document.createElement('button');
         button.setAttribute('class','remove');
-        span.textContent = val;
+        li.textContent = cursor.value.body;
         button.textContent = 'x';
         li.setAttribute('data-note-id', cursor.value.id);
         button.onclick = rem;
-        li.appendChild(span);
         li.appendChild(button);
         ul.appendChild(li);
-        input.value = '';
         input.focus();
         i++;
         cursor.continue();
-       }
-       else
-       {
-        alert(`Either Limit reached delete Some TODO's or NUll value entered`);
-        input.value = '';
-        i--;
-       }
+       
     } 
     else
     {
@@ -100,8 +90,8 @@ window.onload = function()
     
   function rem(e) {
     let noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
-    let transaction = db.transaction(['notes_os'], 'readwrite');
-    let objectStore = transaction.objectStore('notes_os');
+    let transaction = db.transaction(['db'], 'readwrite');
+    let objectStore = transaction.objectStore('db');
     let request = objectStore.delete(noteId);
     transaction.oncomplete = function() {
       e.target.parentNode.parentNode.removeChild(e.target.parentNode);
